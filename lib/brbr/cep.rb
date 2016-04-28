881353b5bfd125f020dac8d81a5a585d7a334c94
stjhimy <stjhimy@gmail.com>
2016-04-28 19:50:02 -0300

    rubocop --auto-correct

--- lib/brbr/cep.rb
+++ lib/brbr/cep.rb
@@ -23,33 +23,33 @@ class CEP
     end
   end
 
-  WEB_SERVICE_REPUBLICA_VIRTUAL_URL = "http://cep.republicavirtual.com.br/web_cep.php?formato=query_string&cep="
+  WEB_SERVICE_REPUBLICA_VIRTUAL_URL = 'http://cep.republicavirtual.com.br/web_cep.php?formato=query_string&cep='.freeze
 
-  # Retorna um hash com os dados de endereçamento para o cep informado ou 
-  # um erro quando o serviço está indisponível, quando o cep informado possui 
+  # Retorna um hash com os dados de endereçamento para o cep informado ou
+  # um erro quando o serviço está indisponível, quando o cep informado possui
   # um formato inválido ou quando o endereço não foi encontrado.
   #
   # Exemplo:
   #  BuscaEndereco.cep(22640100) ==> {:tipo_logradouro => 'Avenida', :logradouro => 'das Américas', :bairro => 'Barra da Tijuca', :uf => 'RJ', :cidade => 'Rio de Janeiro', :cep => '22640100'}
   def self.busca(numero)
     cep = numero.to_s.gsub(/[\.-]/, '')
-    raise "O CEP informado possui um formato inválido." unless cep.to_s.match(/^\d{8}$/)
-    
-    response = Net::HTTP.Proxy(self.proxy_addr, self.proxy_port).get_response(URI.parse("#{WEB_SERVICE_REPUBLICA_VIRTUAL_URL}#{cep}"))
-    raise "A busca de endereço por CEP através do web service da República Virtual está indisponível." unless response.kind_of?(Net::HTTPSuccess)
+    raise "O CEP informado possui um formato inválido." unless cep.to_s =~ /^\d{8}$/
 
-    doc = Hash[* CGI::parse(response.body).map {|k,v| [k,v[0]]}.flatten]
-    
+    response = Net::HTTP.Proxy(proxy_addr, proxy_port).get_response(URI.parse("#{WEB_SERVICE_REPUBLICA_VIRTUAL_URL}#{cep}"))
+    raise "A busca de endereço por CEP através do web service da República Virtual está indisponível." unless response.is_a?(Net::HTTPSuccess)
+
+    doc = Hash[* CGI.parse(response.body).map { |k, v| [k, v[0]] }.flatten]
+
     retorno = {}
-    
-    raise "CEP #{cep} não encontrado." unless [1,2].include?(doc['resultado'].to_i)
+
+    raise "CEP #{cep} não encontrado." unless [1, 2].include?(doc['resultado'].to_i)
 
     %w(tipo_logradouro logradouro bairro cidade uf).each do |field|
       retorno[field.to_sym] = if RUBY_VERSION < '1.9'
-        require 'iconv'
-        Iconv.conv("utf-8", "ISO-8859-1", doc[field])
-      else 
-        doc[field].force_encoding("ISO-8859-1").encode("UTF-8")
+                                require 'iconv'
+                                Iconv.conv('utf-8', 'ISO-8859-1', doc[field])
+                              else
+                                doc[field].force_encoding('ISO-8859-1').encode('UTF-8')
       end
     end
 
--- spec/cep_spec.rb
+++ spec/cep_spec.rb
@@ -3,31 +3,31 @@ require 'fakeweb'
 
 require 'net/http'
 
-INVALID_ZIPS = [0, '0', '00', '000', '0000', '00000', '000000', '0000000', '4006000']
-VALID_ZIPS = [22640100, '22640100', '22640100', '22640100', '22640100']
-VALID_ZIPS_WITH_ZERO_AT_BEGINNING = ['05145100', '05145-100', '05.145-100']
+INVALID_ZIPS = [0, '0', '00', '000', '0000', '00000', '000000', '0000000', '4006000'].freeze
+VALID_ZIPS = [22_640_100, '22640100', '22640100', '22640100', '22640100'].freeze
+VALID_ZIPS_WITH_ZERO_AT_BEGINNING = ['05145100', '05145-100', '05.145-100'].freeze
 
 URL = CEP::WEB_SERVICE_REPUBLICA_VIRTUAL_URL
 
 describe CEP do
-  it "test_raise_without_service_on_both_web_services" do
-    FakeWeb.register_uri(:get, "#{URL}#{22640100}", :status => 504, :body => "Service Unavailable")
-    expect{CEP.busca(22640100)}.to raise_error RuntimeError
+  it 'test_raise_without_service_on_both_web_services' do
+    FakeWeb.register_uri(:get, "#{URL}22640100", status: 504, body: 'Service Unavailable')
+    expect { CEP.busca(22_640_100) }.to raise_error RuntimeError
   end
 
   INVALID_ZIPS.each do |invalid_zip|
     it "test_raise_for_invalid_zip_code_#{invalid_zip}" do
-      expect{CEP.busca(invalid_zip)}.to raise_error(RuntimeError, 'O CEP informado possui um formato inválido.')
+      expect { CEP.busca(invalid_zip) }.to raise_error(RuntimeError, 'O CEP informado possui um formato inválido.')
     end
   end
 
   VALID_ZIPS.each do |valid_zip|
-   it "test_valid_code_#{valid_zip}" do
+    it "test_valid_code_#{valid_zip}" do
       cep = valid_zip
-      expected = {:tipo_logradouro => 'Avenida', :logradouro => 'das Américas', :bairro => 'Barra da Tijuca', :cidade => 'Rio de Janeiro', :uf => 'RJ', :cep => cep.to_s}
-      body = "&resultado=1&resultado_txt=sucesso+-+cep+completo&uf=RJ&cidade=Rio+de+Janeiro&bairro=Barra+da+Tijuca&tipo_logradouro=Avenida&logradouro=das+Am%E9ricas"
+      expected = { tipo_logradouro: 'Avenida', logradouro: 'das Américas', bairro: 'Barra da Tijuca', cidade: 'Rio de Janeiro', uf: 'RJ', cep: cep.to_s }
+      body = '&resultado=1&resultado_txt=sucesso+-+cep+completo&uf=RJ&cidade=Rio+de+Janeiro&bairro=Barra+da+Tijuca&tipo_logradouro=Avenida&logradouro=das+Am%E9ricas'
 
-      FakeWeb.register_uri(:get, "#{URL}#{cep}", :body => body)
+      FakeWeb.register_uri(:get, "#{URL}#{cep}", body: body)
 
       expect(expected).to eq CEP.busca(valid_zip)
     end
@@ -36,20 +36,20 @@ describe CEP do
   VALID_ZIPS_WITH_ZERO_AT_BEGINNING.each do |valid_zip|
     it "test_valid_code_#{valid_zip}" do
       cep = VALID_ZIPS_WITH_ZERO_AT_BEGINNING.first
-      expected = {:tipo_logradouro => "Avenida", :logradouro => "Raimundo Pereira de Magalhães", :bairro => "Jardim Iris", :cidade => "São Paulo", :uf => "SP", :cep => cep}
-      body = "&resultado=1&resultado_txt=sucesso+-+cep+completo&uf=SP&cidade=S%E3o+Paulo&bairro=Jardim+Iris&tipo_logradouro=Avenida&logradouro=Raimundo+Pereira+de+Magalh%E3es"
+      expected = { tipo_logradouro: 'Avenida', logradouro: "Raimundo Pereira de Magalhães", bairro: 'Jardim Iris', cidade: "São Paulo", uf: 'SP', cep: cep }
+      body = '&resultado=1&resultado_txt=sucesso+-+cep+completo&uf=SP&cidade=S%E3o+Paulo&bairro=Jardim+Iris&tipo_logradouro=Avenida&logradouro=Raimundo+Pereira+de+Magalh%E3es'
 
-      FakeWeb.register_uri(:get, "#{URL}#{cep}", :body => body)
+      FakeWeb.register_uri(:get, "#{URL}#{cep}", body: body)
 
       expect(expected).to eq CEP.busca(cep)
     end
   end
 
-  it "test_should_raise_exception_when_invalid_code" do
-    cep = "12345678"
-    body = "&resultado=0&resultado_txt=servi%E7o+indispon%EDvel%2Fcep+inv%E1lido&uf=&cidade=&bairro=&tipo_logradouro=&logradouro="
+  it 'test_should_raise_exception_when_invalid_code' do
+    cep = '12345678'
+    body = '&resultado=0&resultado_txt=servi%E7o+indispon%EDvel%2Fcep+inv%E1lido&uf=&cidade=&bairro=&tipo_logradouro=&logradouro='
 
-    FakeWeb.register_uri(:get, "#{URL}#{cep}", :body => body)
-    expect{CEP.busca(cep)}.to raise_error(RuntimeError, "CEP #{cep} não encontrado.")
+    FakeWeb.register_uri(:get, "#{URL}#{cep}", body: body)
+    expect { CEP.busca(cep) }.to raise_error(RuntimeError, "CEP #{cep} não encontrado.")
   end
 end
